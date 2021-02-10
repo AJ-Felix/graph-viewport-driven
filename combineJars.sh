3eaf38542552f49fa378c435112fe488ca80c48d
AJ-Felix <aljoscha93@web.de>
2021-02-10 10:42:13 +0100

    jar combine script added

---
+++ combineJars.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+(cd tmp; unzip -uo /home/aljoscha/remoteEnvJars/gradoop-flink-0.5.2.jar)
+(cd tmp; unzip -uo /home/aljoscha/remoteEnvJars/gradoop-common-0.5.2.jar)
+(cd tmp; unzip -uo /home/aljoscha/remoteEnvJars/flink-gelly_2.11-1.7.2.jar)
+(cd tmp; unzip -uo /home/aljoscha/remoteEnvJars/viewportDrivenGraphStreaming.jar)
+jar -cvf combined.jar -C tmp .
+rm -r /home/aljoscha/remoteEnvJars/tmp/*
